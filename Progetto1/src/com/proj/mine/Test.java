849c6c1dda693d5a9e84f083a7fd8a4f97a0d7ee
Flavio <name@mail.com>
2016-09-29 09:57:26 +0200

    Modified output message

--- Progetto1/src/com/proj/mine/Test.java
+++ Progetto1/src/com/proj/mine/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Person p = new Person("Flavio", 1986);
-		System.out.println("Name: " + p.getName() + "\nAge: " + p.getAge());
+		System.out.println("Name: " + p.getName() + "\tAge: " + p.getAge());
 	}
 
 }
