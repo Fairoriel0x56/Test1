37d019dfea00510a6d636a62a8eeba68dce17078
Flavio <name@mail.com>
2016-09-29 10:00:55 +0200

    for loop added

--- Progetto1/src/com/proj/mine/Test.java
+++ Progetto1/src/com/proj/mine/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		Person p = new Person("Flavio", 1986);
 		System.out.println("Name: " + p.getName() + "\tAge: " + p.getAge());
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello" + i);
+		}
 	}
 
 }
