5ce859cbf9c4f6058077e821eeacc291efed6c58
Fairoriel0x56 <flavio_f@hotmail.com>
2020-01-22 20:47:12 +0100

    System.out.println() added.

--- Progetto1/src/com/proj/mine/Test.java
+++ Progetto1/src/com/proj/mine/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Person p = new Person("Flavio", 1986);
 		System.out.println("Name: " + p.getName() + "\tAge: " + p.getAge());
+		System.out.println(p);
 		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Hello" + i);
