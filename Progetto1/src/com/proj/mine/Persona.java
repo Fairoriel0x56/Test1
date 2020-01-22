8ef263ad0eb19ace5be5f458a8c3903ee7ad4480
flavio <flavio@xffd.it>
2020-01-22 21:17:57 +0100

    Aggiunte interfaccia Persona e relativa implementazione.

---
+++ Progetto1/src/com/proj/mine/Persona.java
@@ -0,0 +1,9 @@
+package com.proj.mine;
+
+public interface Persona {
+	
+	public String getNome();
+	
+	public String getCognome();
+
+}
---
+++ Progetto1/src/com/proj/mine/PersonaImpl.java
@@ -0,0 +1,15 @@
+package com.proj.mine;
+
+public class PersonaImpl implements Persona {
+
+	@Override
+	public String getNome() {
+		return "Nome";
+	}
+
+	@Override
+	public String getCognome() {
+		return "Cognome";
+	}
+
+}
